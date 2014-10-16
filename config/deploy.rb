a4b391f25be20d21915c3b97dd35e2c48cb7c6dd
Emmanuel BOTROS YOUSSEF <emmanuel.botros@gmail.com>
2014-10-16 13:40:18 +0200

    adding fpm conf deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,27 +14,31 @@ set :linked_dirs, %w{node_modules}
 set :keep_releases, 12
 set :grunt_flags, '--no-color'
 set :composer_install_flags, ''
-  task :release_authconf do
-    on roles :all do
-        sudo "echo \"installs git auth conf (not in the repo!)\""
-        sudo "cp /vagrant/auth.json /home/vagrant/.composer/auth.json"
-    end
-  end
 
   task :release_installation do
     on roles :all do
         sudo "echo \"Fetches and installs release folder for this app\""
         sudo "rm -f /var/www/release && cdir=$(ls -td /var/www/ws-orpgl/releases/* | head -n1) && sudo ln -s $cdir /var/www/release"
+        sudo "chmod -R 777 /var/www/release/web"
     end
   end
+
+ task :release_fpm do
+    on roles :all do
+        sudo "echo \"Install fpm configuration file\""
+        sudo "cp /vagrant/www.conf.sav /etc/php5/fpm/pool.d/www.conf"
+  end
+ end
 
  task :release_reload do
     on roles :all do
         sudo "echo \"Restarts webserver+fpm\""
+	#sudo "ln -fs /vagrant/config/nginx.conf /etc/nginx/sites-enabled/default"
         sudo "service php5-fpm restart && sudo chmod go+rw /var/run/php5-fpm.sock && sudo service nginx restart"
     end
   end
-after 'deploy:updated', 'release_authconf'
+
 after 'deploy:updated', 'composer:install'
 after 'deploy:updated', 'release_installation'
+after 'deploy:updated', 'release_fpm'
 after 'deploy:updated', 'release_reload'
